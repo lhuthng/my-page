bc8d7c2c42610922fc649e7e2f3daa8fccfd7393
Huu Thang, Le <62278351+lhuthng@users.noreply.github.com>
2025-09-19 07:51:18 +0200

    Add work experience

--- portfolio/src/Components/WorkExperience.tsx
+++ portfolio/src/Components/WorkExperience.tsx
@@ -1,21 +1,82 @@
+import type { ReactNode } from "react"
+import { Children, useEffect, useLayoutEffect, useRef, useState } from "react"
+import '@/Styles/Experience.css'
+import gsap from "gsap"
+
 interface ExperienceInfoProps {
     title: string,
     duration: string
     organization: string,
-    location: string,
-    description: string
+    location?: string,
+    technologies: string[],
+    project: string,
+    additional?: ReactNode[],
+    children: ReactNode
 }
 
 export default function ExperienceInfo({
-    title, duration, organization, location, description
+    title, duration, organization, technologies, project, location, additional = [], children
 }: ExperienceInfoProps) {
-    return (<div className="flex flex-col border-2 p-4 w-full">
-        <h1>{title}</h1>
-        <span>{organization}</span>
-        <span className="flex justify-between">
-            <span>{location}</span>
-            <span>{duration}</span>
-        </span>
-        <p>{description}</p>
+    const [ expanded, setExpanded ] = useState(false);
+    const borderRef = useRef<HTMLDivElement>(null);
+    const slotRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        gsap.utils.toArray(".custom-border").forEach((border) => {
+            gsap.to(border as Element, {
+                scrollTrigger: {
+                    trigger: border as Element,
+                    start: "bottom bottom",
+                    toggleClass: "is-visible",
+                    once: true
+                }
+            });
+        });
+        gsap.utils.toArray(".details-slot").forEach(slot => {
+            gsap.to(slot as Element, {
+                scrollTrigger: {
+                    trigger: slot as Element,
+                    start: "top bottom",
+                    toggleClass: "is-visible",
+                    once: true
+                }
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        borderRef.current?.classList.toggle('open', expanded);
+        slotRef.current?.classList.toggle('open', expanded);
+    }, [expanded]);
+
+    return (<div className={`cell-adjust w-full space-y-2 bg-blackboard z-0}`}>
+        <div ref={borderRef} className="custom-border z-11"><i/><i/></div>
+        <div className="relative m-0 pb-2 bg-blackboard z-10">
+            <div className="relative p-4 pb-2">
+                <div className="relative w-full">
+                    <div className="absolute inset-y-1/2 w-full h-[2px] bg-white z-0"></div>
+                    <div className="relative title flex font-bold text-2xl w-fit bg-blackboard z-10">{title}</div>
+                </div>
+                <div className="flex justify-between italic">
+                    <div><div>{organization}</div><div>{location ?? "(remote)"}</div></div>
+                    <span className="text-gray-chalk">{duration}</span>
+                </div>
+                <div><b>Technologies:</b> {technologies.join(', ')}.</div>
+                <div><b>Project:</b> {project}</div>
+            </div>        
+            <div className="px-4"><b>Contributions:</b> <span className="text-xs rounded-full border-1 px-1 cursor-pointer hover:brightness-140 transition-all duration-100" 
+                style={{
+                    borderColor: "var(--color-gray-chalk)",
+                    color: expanded ? "var(--color-blackboard)" : "var(--color-gray-chalk)",
+                    backgroundColor: expanded ? "var(--color-gray-chalk)" : "transparent"
+                }}
+                onClick={() => setExpanded(expanded => !expanded)}
+            >{expanded ? "collapse" : "expand" }</span></div>
+        </div>          
+        <div ref={slotRef} className="details-slot overflow-y-hidden z-9">
+            <ul className="contributions-marker space-y-2 pl-10 pb-2">
+                {Children.map(children, (child, index) => <li key={index}>{child}</li>)}
+            </ul>
+        </div>
     </div>)
 }
