ad39db97d7f44c7c5c13236ed5fb4de592a936ee
Thang <thang@Mac-mini-von-Thang.local>
2025-11-07 10:00:44 +0100

    fix refresh token

---
+++ Blog/frontend/src/lib/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<header class="fixed w-full h-18 bg-gray-200">
+	<div class="flex w-full justify-between p-4">
+		<div class="bg-red-500 rounded-full w-10 h-10"></div>
+		<div class="flex items-center gap-8">
+			<span>Tags</span>
+			<span>Archives</span>
+			<span>Portfolio</span>
+			<span>About</span>
+			<button class="ring-1 rounded-full px-2">Login</button>
+		</div>
+	</div>
+</header>
