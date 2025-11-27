8999f9b7b4df5401c132f49da307738c5a719877
Thang <lhuthng@gmail.com>
2025-11-27 13:37:09 +0100

    Refactor: change posteditor to postuploader

---
+++ Blog/frontend/src/lib/components/post/PostRenderer.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { mediaWithShortcutPlugin } from "$lib/custom-rules";
+    import { useDebounce } from "$lib/effects/debounce";
+    import MarkdownIt from "markdown-it";
+
+    let { text, mediaDictionary } = $props();
+
+    let md = $derived(
+        new MarkdownIt().use(mediaWithShortcutPlugin, { mediaDictionary }),
+    );
+
+    let renderedText = $derived(md.render(text));
+</script>
+
+<div class="rendered-markdown">
+    {@html renderedText}
+</div>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+
+    :global(.rendered-markdown) {
+        & h1 {
+        }
+        & img {
+            @apply rounded-lg mx-auto;
+        }
+    }
+</style>
