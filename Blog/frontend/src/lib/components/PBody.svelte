6872b49525b23c6ee272b10c73a6c1cc7a82230e
Thang <lhuthng@gmail.com>
2025-12-08 08:46:03 +0100

    Add changing avatar

---
+++ Blog/frontend/src/lib/components/PBody.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { pbody } from "$lib/client/misc";
+    import Portal from "./Portal.svelte";
+
+    let { children } = $props();
+</script>
+
+<Portal
+    class={`full bg-dark/50 ${!children ? "hidden" : "block"}`}
+    target={$pbody}
+>
+    {@render children?.()}
+</Portal>
