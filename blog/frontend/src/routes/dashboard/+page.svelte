b424ca37c3ac8ce069826487730a477c96ece866
Thang <lhuthng@gmail.com>
2025-12-31 18:11:23 +0100

    fix docker refresh token

--- blog/frontend/src/routes/dashboard/+page.svelte
+++ blog/frontend/src/routes/dashboard/+page.svelte
@@ -2,41 +2,41 @@
 </script>
 
 <div class="flex flex-col gap-2 xl:gap-4 relative full">
-    <div class="stat-grid grid-cols-2 xl:grid-cols-4 gap-2 xl:gap-4">
-        <div>
-            <h4>Total views</h4>
-            <p>0</p>
-        </div>
-        <div>
-            <h4>Total views</h4>
-            <p>0</p>
-        </div>
-        <div>
-            <h4>Total views</h4>
-            <p>0</p>
-        </div>
-        <div>
-            <h4>Total views</h4>
-            <p>0</p>
-        </div>
+  <div class="stat-grid grid-cols-2 xl:grid-cols-4 gap-2 xl:gap-4">
+    <div>
+      <h4>Total views</h4>
+      <p>0</p>
     </div>
-    <div class="w-full p-4 bg-white/80 rounded-xl">
-        <h3>Recent Activity</h3>
+    <div>
+      <h4>Total views</h4>
+      <p>0</p>
     </div>
+    <div>
+      <h4>Total views</h4>
+      <p>0</p>
+    </div>
+    <div>
+      <h4>Total views</h4>
+      <p>0</p>
+    </div>
+  </div>
+  <div class="w-full p-4 bg-white/80 rounded-xl">
+    <h3>Recent Activity</h3>
+  </div>
 </div>
 
 <style lang="postcss">
-    @reference "../../app.css";
+  @reference "../../app.css";
 
-    .stat-grid {
-        @apply grid *:col-span-1 *:rounded-xl *:bg-white/80 *:p-4;
-    }
+  .stat-grid {
+    @apply grid *:col-span-1 *:rounded-xl *:bg-white/80 *:p-4;
+  }
 
-    h4 {
-        @apply text-lg font-semibold;
-    }
+  h4 {
+    @apply text-lg font-semibold;
+  }
 
-    h3 {
-        @apply text-xl font-semibold;
-    }
+  h3 {
+    @apply text-xl font-semibold;
+  }
 </style>
