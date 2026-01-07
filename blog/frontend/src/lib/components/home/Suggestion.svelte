193f077151d326a00711f9ba3417b57244f937b5
Thang <lhuthng@gmail.com>
2026-01-07 04:16:47 +0100

    ended

--- blog/frontend/src/lib/components/home/Suggestion.svelte
+++ blog/frontend/src/lib/components/home/Suggestion.svelte
@@ -13,9 +13,11 @@
     <li>Add a Projects page</li>
     <li>Add an About page</li>
     <li>
-      Add a Kaomoji shortcut? Why not? <span class="text-nowrap">(◕‿◕)♡</span>
+      Add Kaomoji shortcuts? Why not? <span class="text-nowrap">(◕‿◕)♡</span>
     </li>
-    <li class="line-through">Fix the annoyed 413 error</li>
+    <li class="line-through">
+      Fix annoying 413 errors (will write post about this)
+    </li>
     <li class="line-through">Fix rate limit</li>
   </ul>
 </div>
