a2d8462d7f5ed5fe6647c5eef9254c582c8dccce
Thang <lhuthng@gmail.com>
2026-01-14 06:42:57 +0100

    update glb-demo

--- blog/frontend/build/server/chunks/state.svelte-CeWcm6KF.js
+++
@@ -1,8 +0,0 @@
-import { noop } from "./utils2-B3oc7zZR.js";
-
-//#region .svelte-kit/adapter-bun/chunks/state.svelte.js
-const is_legacy = noop.toString().includes("$$") || /function \w+\(\) \{\}/.test(noop.toString());
-if (is_legacy) new URL("https://example.com");
-
-//#endregion
-//# sourceMappingURL=state.svelte-CeWcm6KF.js.map
