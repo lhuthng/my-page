8deef8e99ce46ce14b4b39fb93c2110aed3a3559
Thang <lhuthng@gmail.com>
2026-01-15 09:49:24 +0100

    fix search media

---
+++ blog/frontend/src/routes/api/media/all/+server.js
@@ -0,0 +1,18 @@
+import { proxyFallback, route, fixClientRoute } from "$lib/server/proxy.js";
+
+export async function GET({ request, fetch, url }) {
+  let res = await proxyFallback({
+    request,
+    params: { path: "media/all" },
+    search: url.search,
+  });
+
+  if (!res.ok) {
+    const text = await res.text();
+    return new Response(text, { status: res.status });
+  }
+
+  const { results } = await res.json();
+  results.forEach((result) => (result.url = fixClientRoute(result.url)));
+  return new Response(JSON.stringify({ results }), { status: 200 });
+}
