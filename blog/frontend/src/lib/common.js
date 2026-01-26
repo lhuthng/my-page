ab4a00c274f57f7695f8b004a7c0acf0c216c894
Thang <lhuthng@gmail.com>
2026-01-26 01:44:58 +0100

    add table of contents button

--- blog/frontend/src/lib/common.js
+++ blog/frontend/src/lib/common.js
@@ -1,6 +1,10 @@
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 
+export const widthThreshold = {
+  lg: 1024,
+};
+
 export function preventDefault(e) {
   e.preventDefault();
 }
