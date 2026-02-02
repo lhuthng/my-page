6f739dfc87719aa7cddda9ef79f47823fdcfffa5
Thang <lhuthng@gmail.com>
2026-02-02 19:05:07 +0100

    fix comment image

--- blog/frontend/src/lib/components/home/Suggestion.svelte
+++ blog/frontend/src/lib/components/home/Suggestion.svelte
@@ -4,22 +4,14 @@
 <div class="w-60 px-4 py-2">
   <h3 class="font-semibold">To do list (for this blog):</h3>
   <ul class="list-disc list-inside">
-    <li class="line-through">Implement Threlte</li>
     <li>Fill in post content</li>
-    <li class="line-through">Add series feature</li>
     <li>Add post filters</li>
-    <li>
-      <span class="line-through"
-        >Set up email service for user registration and contact form</span
-      >
-    </li>
-    <li>Redesign the footer</li>
     <li>Add the Projects page</li>
-    <li class="line-through">Add the About page</li>
     <li>Set up newsletter</li>
     <li>
       Add Kaomoji shortcuts? Why not? <span class="text-nowrap">(◕‿◕)♡</span>
     </li>
+    <li>Add a Serverless Proxy for GIF search</li>
     <li class="line-through">
       Fix annoying 413 errors (will write post about this)
     </li>
