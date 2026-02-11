0f6172af680e5814a5760d43e8e8b79fe1613760
Thang <lhuthng@gmail.com>
2026-02-11 07:43:45 +0100

    add a navigator for the table of content

--- blog/frontend/src/lib/components/post/ContentTable.svelte
+++ blog/frontend/src/lib/components/post/ContentTable.svelte
@@ -1,7 +1,55 @@
 <script>
+  import { onMount } from "svelte";
   import PBody from "../PBody.svelte";
+  import { browser } from "$app/environment";
 
   let { headers } = $props();
+  let ids = $derived.by(() => {
+    let ids = [];
+    const traverse = (array) => {
+      array.forEach((element) => {
+        if (Array.isArray(element)) {
+          traverse(element);
+        } else {
+          ids.push(element.id);
+        }
+      });
+    };
+    traverse(headers);
+    return ids;
+  });
+
+  let active = $state();
+
+  function updateActive() {
+    const scrollTop = window.scrollY;
+    const viewportHeight = window.innerHeight;
+
+    let firstVisible = null;
+
+    for (const id of ids) {
+      const el = document.getElementById(id);
+      if (!el) continue;
+
+      const rect = el.getBoundingClientRect();
+      if (rect.bottom > 0) {
+        if (!firstVisible || rect.top < firstVisible.rectTop) {
+          firstVisible = { id: id, rectTop: rect.top };
+        }
+      }
+    }
+
+    if (firstVisible) active = firstVisible.id;
+  }
+
+  onMount(() => {
+    if (!browser) return;
+
+    updateActive();
+    window.addEventListener("scroll", updateActive);
+
+    return () => window.removeEventListener("scroll", updateActive);
+  });
 </script>
 
 {#snippet toList(array)}
@@ -11,7 +59,9 @@
         {@render toList(element)}
       {:else}
         {@const { id, textContent } = element}
-        <li><a href={`#${id}`}>{textContent}</a></li>
+        <li>
+          <a class:active={id === active} href={`#${id}`}>{textContent}</a>
+        </li>
       {/if}
     {/each}
   </ol>
@@ -22,6 +72,10 @@
 
 <style lang="postcss">
   @reference "../../../app.css";
+
+  .active {
+    @apply underline text-accent-blue;
+  }
 
   ol {
     counter-reset: item;
