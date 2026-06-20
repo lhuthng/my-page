e0f8206ed77187122d918d72130cdc851842d559
Thang <lhuthng@gmail.com>
2026-06-20 18:05:41 +0200

    chore: fix undefined class

--- blog/frontend/src/lib/components/home/Suggestion.svelte
+++ blog/frontend/src/lib/components/home/Suggestion.svelte
@@ -2,9 +2,10 @@
 </script>
 
 <div class="w-60 px-4 py-2">
-	<h3 class="font-semibold">To do list (for this blog):</h3>
+	<h3 class="font-semibold">To-do list (for this blog):</h3>
 	<ul class="list-disc list-inside">
 		<li>Set up newsletter</li>
+		<li class="line-through">Add aliases for commenting</li>
 		<li class="line-through">
 			Add Kaomoji shortcuts? Why not? <span class="text-nowrap">(◕‿◕)♡</span>
 		</li>
