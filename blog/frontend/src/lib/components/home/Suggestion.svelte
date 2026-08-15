529b71a67ddb9ff4ef91560b114926b0f25f8414
Thang <lhuthng@gmail.com>
2026-08-15 15:41:55 +0200

    chore: tailor footer newsletter styling and home to-do list
    Trim the newsletter blurb, tighten the footer input border/placeholder and
    green Subscribe button, and mark newsletter + v86 save/load as done on the
    home to-do list.

--- blog/frontend/src/lib/components/home/Suggestion.svelte
+++ blog/frontend/src/lib/components/home/Suggestion.svelte
@@ -4,12 +4,13 @@
 <div class="w-60 px-4 py-2">
 	<h3 class="font-semibold">To-do list (for this blog):</h3>
 	<ul class="list-disc list-inside">
-		<li>Set up newsletter</li>
-		<li>Add save/load for v86 games</li>
+		<li class="line-through">Set up newsletter</li>
+		<li class="line-through">Add save/load for v86 games</li>
 		<li class="line-through">Add aliases for commenting</li>
 		<li class="line-through">
 			Add Kaomoji shortcuts? Why not? <span class="text-nowrap">(◕‿◕)♡</span>
 		</li>
 		<li class="line-through">Set up SMTP</li>
+		<li>Hmmm, nothing left? Imma put catching 🐞 then!</li>
 	</ul>
 </div>
